592e13e16e1516502adacd7dbe1691313ec6c9a4
Jordan <jordanleelong5@gmail.com>
2020-03-05 20:27:48 -0800

    adding validation

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -2,6 +2,7 @@ class WorkoutsController < ApplicationController
 
   # GET: /workouts
   get "/workouts" do
+    @new_workout = Workout.all
     erb :"/workouts/index.html"
   end
 
@@ -12,22 +13,37 @@ class WorkoutsController < ApplicationController
 
   # POST: /workouts
   post "/workouts" do
-    redirect "/workouts"
+    workout = Workout.create(params)
+    redirect "/workouts/#{workout.id}"
   end
 
   # GET: /workouts/5
   get "/workouts/:id" do
+    @workout = Workout.find(params[:id])
+    if current_user == @workout.user
     erb :"/workouts/show.html"
+    else  
+      redirect to '/'
+    end
   end
 
   # GET: /workouts/5/edit
   get "/workouts/:id/edit" do
+    if logged_in?
+    @workout = Workout.find(params[:id])
     erb :"/workouts/edit.html"
+    else
+   redirect to '/'
+    end
   end
 
   # PATCH: /workouts/5
   patch "/workouts/:id" do
-    redirect "/workouts/:id"
+    #find the workout dynamically aka by id
+    @workout = Workout.find(params[:id])
+    @workout.update(params[:id])
+    #update it via .update
+    redirect "/workouts/#{@workout.id}"
   end
 
   # DELETE: /workouts/5/delete
