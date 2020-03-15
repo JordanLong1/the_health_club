67a5594dc9fa50a0e217ad539b1215e6e59c3f8e
Jordan <jordanleelong5@gmail.com>
2020-03-15 14:03:34 -0700

    ready for assessment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
     has_many :workouts
     has_secure_password
-    validates :email, :username, :password_digest, presence: true 
+    validates_uniqueness_of :email, :username, :password_digest, presence: true 
 end
